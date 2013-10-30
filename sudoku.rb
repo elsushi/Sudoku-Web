4c41e96390d8b6b5a96c2d8cc0dcc0398146b2fe
Arturo Ortiz <elsushi@gmail.com>
2013-10-30 15:38:43 +0000

    Added the capacity to hide numbers in the puzzle

--- sudoku.rb
+++ sudoku.rb
@@ -11,7 +11,22 @@ def random_sudoku
 	sudoku.to_s.chars
 end
 
+def puzzle(sudoku)
+	mangled_sudoku = sudoku.dup
+	(0..80).to_a.sample(rand(30)).each do |index|
+		mangled_sudoku[index] = 0
+	end
+	mangled_sudoku
+end
+
 get '/' do
-	@current_solution = random_sudoku 
+	sudoku = random_sudoku
+	session[:solution] = sudoku
+	@current_solution = puzzle(sudoku)
+	erb :index
+end
+
+get '/solution' do
+	@current_solution = session[:solution]
 	erb :index
 end
